a0d3f2fece1675e6a72176e5a869cf637c65fc61
Ticiano <ticiano.fracette@estudante.ufjf.br>
2023-11-29 17:53:59 -0300

    Parte 1 - Adaptando código para digrafos

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,73 +1,90 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 #include <vector>
 
 #include "TabelaIncidencia.h"
 
 using namespace std;
 
-void LeMatriz(ifstream &input_file, vector<vector<int>> &matriz, int n);
-void CriaMatrizIncidencia(vector<vector<int>> &matrizInc, const vector<vector<int>> &matrizAdj, int n);
-TabelaIncidencia CriaTabelaIncidencia(const vector<vector<int>> &matrizInc);
-void ImprimeMatrizNxN(const vector<vector<int>> &matriz, int n, ofstream &output_file);
-void ImprimeMatrizIncidencia(const vector<vector<int>> &matrizInc, int n, int nArestas, ofstream &output_file);
-
-string diretorio = "../input/grafo.txt";
+void LerMatriz(ifstream &input_file, vector<vector<int>> &matriz, int n);
+vector<vector<int>> CriarMatrizAdjacencia(const vector<vector<int>> &matrizDist);
+vector<vector<int>> CriarMatrizIncidencia(const vector<vector<int>> &matrizDist, int tipoGrafo);
+TabelaIncidencia CriarTabelaIncidencia(const vector<vector<int>> &matrizInc);
+void ImprimirMatrizNxN(const vector<vector<int>> &matriz, int n, ofstream &output_file);
+void ImprimirMatrizIncidencia(const vector<vector<int>> &matrizInc, int n, ofstream &output_file);
+void Imprimir(const vector<vector<int>> &matrizAdj, const vector<vector<int>> &matrizInc, TabelaIncidencia *tabelaInc, int n, ofstream &output_file);
 
 int main()
 {
-    ifstream input_file(diretorio);
+    // Abre o arquivo de entrada
+    ifstream input_file("../input/grafo.txt");
 
-    if(!input_file.is_open()){
+    // Abre/cria o arquivo de saída
+    ofstream output_file("../output/saida.txt");
+
+    if (!input_file.is_open())
+    {
         cout << "Não foi possível abrir o arquivo." << endl;
         return 1;
     }
 
     // Leitura número de vértices
-    int n; 
+    int n;
     input_file >> n;
 
-    // Leitura da matriz de adjacência
+    // Matriz a ser lida (adjadência ou distância)
+    vector<vector<int>> matriz;
+    LerMatriz(input_file, matriz, n);
+
     vector<vector<int>> matrizAdj;
-    LeMatriz(input_file, matrizAdj, n);
+    vector<vector<int>> matrizInc;
+    TabelaIncidencia tabelaInc;
 
-    // Leitura da matriz de distância
-    vector<vector<int>> matrizDist;
-    LeMatriz(input_file, matrizDist, n);
+    int tipoGrafo; // 1 - Simples (matriz lida = adjacência), 2 - Digrafo não-valorado (matriz lida = adjacência), 3 - Digrafo valorado (matriz lida = distância)
+    int opcao;
 
-    // Conta número de arestas do grafo
-    int nArestas = 0;
+    cout << "[1] Grafo Simples, não-valorado" << endl;
+    cout << "[2] Digrafo Simples, não-valorado" << endl;
+    cout << "[3] Digrafo Simples, valorado" << endl << endl;
+    cin >> opcao;
+    cout << endl;
 
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            if(matrizAdj[i][j] == 1) 
-                nArestas++;
+    switch (opcao)
+    {
+        case 1:
+            tipoGrafo = 1;
+            matrizInc = CriarMatrizIncidencia(matriz, tipoGrafo);
+            tabelaInc = CriarTabelaIncidencia(matrizInc);
 
-    // Cria e zera matriz de incidência
-    vector<vector<int>> matrizInc(n, vector<int>(nArestas, 0));
-    CriaMatrizIncidencia(matrizInc, matrizAdj, n);    
+            // Imprime no console e escreve no arquivo
+            Imprimir(matriz, matrizInc, &tabelaInc, n, output_file);
+            break;
+        case 2:
+            tipoGrafo = 2;            
+            matrizInc = CriarMatrizIncidencia(matriz, tipoGrafo);
+            tabelaInc = CriarTabelaIncidencia(matrizInc);
 
-    // Cria tabela de incidência
-    TabelaIncidencia tabelaInc = CriaTabelaIncidencia(matrizInc);
+            // Imprime no console e escreve no arquivo
+            Imprimir(matriz, matrizInc, &tabelaInc, n, output_file);            
+            break;
+        case 3:
+            tipoGrafo = 3;
+            matrizAdj = CriarMatrizAdjacencia(matriz);
+            matrizInc = CriarMatrizIncidencia(matriz, tipoGrafo);
+            tabelaInc = CriarTabelaIncidencia(matrizInc);
 
-    // Imprime no console e escreve no arquivo de saída
-    ofstream output_file("../output/saida.txt"); // Abre/cria o arquivo de saída
-
-    cout << "Matriz de Adjacência:" << endl;
-    output_file << "Matriz de Adjadência:" << endl;
-    ImprimeMatrizNxN(matrizAdj, n, output_file);
-
-    cout << endl << "Matriz de Distância:" << endl;
-    output_file << endl << "Matriz de Distância:" << endl;
-    ImprimeMatrizNxN(matrizDist, n, output_file);
-    
-    cout << endl << "Matriz de Incidência:" << endl << "  ";
-    output_file << endl << "Matriz de Incidência:" << endl << "  ";
-    ImprimeMatrizIncidencia(matrizInc, n, nArestas, output_file);
-
-    cout << endl << "Tabela de Incidência:" << endl;
-    output_file << endl << "Tabela de Incidência:" << endl;
-    tabelaInc.imprimeTabelaInc(output_file);
+            // Imprime no console e escreve no arquivo
+            cout << "Matriz de Distância:" << endl;
+            output_file << "Matriz de Distância:" << endl;
+            ImprimirMatrizNxN(matriz, n, output_file);
+            Imprimir(matriz, matrizInc, &tabelaInc, n, output_file);            
+            break;
+        default:
+            cout << "Erro: Entrada inválida!" << endl;
+            break;
+    }
+    cout << endl;
 
     // Fecha arquivo de saída
     output_file.close();
@@ -75,12 +92,14 @@ int main()
     return 0;
 }
 
-void LeMatriz(ifstream &input_file, vector<vector<int>> &matriz, int n)
+void LerMatriz(ifstream &input_file, vector<vector<int>> &matriz, int n)
 {
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         vector<int> linha;
 
-        for(int j = 0; j < n; j++){
+        for (int j = 0; j < n; j++)
+        {
             int valor;
             input_file >> valor;
             linha.push_back(valor);
@@ -89,71 +108,155 @@ void LeMatriz(ifstream &input_file, vector<vector<int>> &matriz, int n)
     }
 }
 
-void CriaMatrizIncidencia(vector<vector<int>> &matrizInc, const vector<vector<int>> &matrizAdj, int n)
+// Função para criar a matriz de adjacência a partir da matriz de distância
+vector<vector<int>> CriarMatrizAdjacencia(const vector<vector<int>> &matrizDist)
 {
-    int indiceAresta = 0; // Coluna atual
+    int nVertices = matrizDist.size();
+    vector<vector<int>> matrizAdj(nVertices, vector<int>(nVertices, 0));
 
-    for(int i = 0; i < n; i++)
-        for(int j = i + 1; j < n; j++)
-            if(matrizAdj[i][j] == 1){
-                matrizInc[i][indiceAresta] = 1;
-                matrizInc[j][indiceAresta] = 1;
-                indiceAresta++;
+    for (int i = 0; i < nVertices; i++)
+    {
+        for (int j = 0; j < nVertices; j++)
+        {
+            if (matrizDist[i][j] != 0 && matrizDist[i][j] != 999)
+            {
+                matrizAdj[i][j] = 1; // Existe uma aresta entre i e j
             }
+        }
+    }
+
+    return matrizAdj;
 }
 
-TabelaIncidencia CriaTabelaIncidencia(const vector<vector<int>> &matrizInc)
+vector<vector<int>> CriarMatrizIncidencia(const vector<vector<int>> &matriz, int tipoGrafo)
+{
+    int nVertices = matriz.size();
+    int nArestas = 0;
+
+    // Conta o número total de arestas no digrafo
+    for (int i = 0; i < nVertices; i++)
+    {
+        for (int j = 0; j < nVertices; j++)
+        {
+            if (matriz[i][j] != 0 && matriz[i][j] != 999)
+            {
+                nArestas++;
+            }
+        }
+    }
+
+    vector<vector<int>> matrizInc(nVertices, vector<int>(nArestas, 0));
+
+    int arestaAtual = 0;
+    
+    // Preenche a matriz de incidência
+    if(tipoGrafo == 2 || tipoGrafo == 3){
+        for (int j = 0; j < nVertices; j++)
+        {
+            for (int i = 0; i < nVertices; i++)
+            {
+                if (matriz[i][j] != 0 && matriz[i][j] != 999)
+                {
+                    matrizInc[i][arestaAtual] = -1;
+                    matrizInc[j][arestaAtual] = 1;
+                    arestaAtual++;
+                }
+            }
+        }
+    }
+    else if(tipoGrafo == 1){
+       for (int i = 0; i < nVertices; i++)
+        for (int j = i + 1; j < nVertices; j++)
+            if (matriz[i][j] == 1)
+            {
+                matrizInc[i][arestaAtual] = 1;
+                matrizInc[j][arestaAtual] = 1;
+                arestaAtual++;
+            }
+    }
+
+    return matrizInc;
+}
+
+TabelaIncidencia CriarTabelaIncidencia(const vector<vector<int>> &matrizInc)
 {
     int nLinhas = matrizInc.size();
     int nColunas = matrizInc[0].size();
 
     TabelaIncidencia tabelaInc(nLinhas, nColunas);
 
-    for(int i = 0; i < nLinhas; i++)
-        for(int j = 0; j < nColunas; j++)
-            if(matrizInc[i][j] != 0)
-                tabelaInc.addElement(i, j, matrizInc[i][j]);
+    for (int i = 0; i < nLinhas; i++)
+        for (int j = 0; j < nColunas; j++)
+            if (matrizInc[i][j] != 0)
+                tabelaInc.AdicionarElemento(i, j, matrizInc[i][j]);
     return tabelaInc;
 }
 
-void ImprimeMatrizNxN(const vector<vector<int>> &matriz, int n, ofstream &output_file)
+void ImprimirMatrizNxN(const vector<vector<int>> &matriz, int n, ofstream &output_file)
 {
     cout << "  ";
     output_file << "  ";
-    for(int i = 0; i < n; i++){
-        cout << i << " ";
-        output_file << i << " ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << setw(5) << i << " ";
+        output_file << setw(5) << i << " ";
     }
-    cout << endl;
-    output_file << endl;
-    for(int i = 0; i < n; i++){
+    cout << endl
+         << endl;
+    output_file << endl
+                << endl;
+    for (int i = 0; i < n; i++)
+    {
         cout << i << " ";
         output_file << i << " ";
-        for(int j = 0; j < n; j++){
-            cout << matriz[i][j] << " ";
-            output_file << matriz[i][j] << " ";
+        for (int j = 0; j < n; j++)
+        {
+            cout << setw(5) << matriz[i][j] << " ";
+            output_file << setw(5) << matriz[i][j] << " ";
         }
         cout << endl;
         output_file << endl;
     }
 }
 
-void ImprimeMatrizIncidencia(const vector<vector<int>> &matrizInc, int n, int nArestas, ofstream &output_file)
+void ImprimirMatrizIncidencia(const vector<vector<int>> &matrizInc, int n, ofstream &output_file)
 {
-    for(int i = 0; i < nArestas/2; i++){
-        cout << (char)(97+i) << " ";
-        output_file << (char)(97+i) << " ";
+    for (int i = 0; i < matrizInc[0].size(); i++)
+    {
+        cout << setw(5) << (char)(97 + i) << " ";
+        output_file << setw(5) << (char)(97 + i) << " ";
     }
-    cout << endl;
-    output_file << endl;
-    for(int i = 0; i < n; i++){
+    cout << endl
+         << endl;
+    output_file << endl
+                << endl;
+    for (int i = 0; i < n; i++)
+    {
         cout << i << " ";
         output_file << i << " ";
-        for(int j = 0; j < nArestas/2; j++){
-            cout << matrizInc[i][j] << " ";
-            output_file << matrizInc[i][j] << " ";
+        for (int j = 0; j < matrizInc[0].size(); j++)
+        {
+            cout << setw(5) << matrizInc[i][j] << " ";
+            output_file << setw(5) << matrizInc[i][j] << " ";
         }
         cout << endl;
         output_file << endl;
     }
 }
+
+void Imprimir(const vector<vector<int>> &matrizAdj, const vector<vector<int>> &matrizInc, TabelaIncidencia *tabelaInc, int n, ofstream &output_file)
+{
+            cout << endl << "Matriz de Adjacência:" << endl;
+            output_file << endl << "Matriz de Adjadência:" << endl;
+            ImprimirMatrizNxN(matrizAdj, n, output_file);
+
+            cout << endl << "Matriz de Incidência:" << endl << "  ";
+            output_file << endl << "Matriz de Incidência:" << endl  << "  ";
+            ImprimirMatrizIncidencia(matrizInc, n, output_file);
+
+            cout << endl << "Tabela de Incidência:" << endl;
+            output_file << endl << "Tabela de Incidência:" << endl;
+            tabelaInc->Imprimir(output_file);
+}
+
+
--- src/TabelaIncidencia.cpp
+++ src/TabelaIncidencia.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 #include <vector>
+
 #include "TabelaIncidencia.h"
 
 using namespace std;
 
-void TabelaIncidencia::addElement(int linha, int coluna, int valor)
+void TabelaIncidencia::AdicionarElemento(int linha, int coluna, int valor)
 {
     No* novoNo = new No(coluna, valor);
     novoNo->prox = linhas[linha];
     linhas[linha] = novoNo;
 }
 
-void TabelaIncidencia::imprimeTabelaInc(ofstream &output_file)
+void TabelaIncidencia::Imprimir(ofstream &output_file)
 {
     for(int i = 0; i < linhas.size(); i++){
         No* atual = linhas[i];
         while(atual != nullptr){
-            cout << "(" << i << ", " << (char)(atual->coluna + 97) << ") = " << atual->valor << endl;
-            output_file << "(" << i << ", " << (char)(atual->coluna + 97) << ") = " << atual->valor << endl;
+            cout << "(" << i << ", " << (char)(atual->coluna + 97) << ") = " << setw(2) << atual->valor << endl;
+            output_file << "(" << i << ", " << (char)(atual->coluna + 97) << ") = " << setw(2) << atual->valor << endl;
             atual = atual->prox;
         }
     }
